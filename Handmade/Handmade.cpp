a3f6c7e0ef0df3af9ad34ce967b224b64a1a1efc
Alex Simanovsky <SAlechko@gmail.com>
2017-06-19 21:49:02 +0300

    testing

--- Handmade/Handmade.cpp
+++ Handmade/Handmade.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	MessageBoxA(0, "This is handmade", "Handmade", MB_OK | MB_ICONINFORMATION);
+	MessageBoxA(0, "This is cookie", "Handmade", MB_OK | MB_ICONINFORMATION);
 	return 0;
 }
